fb710a408a031bdd93fec60a599e7f2fb721bb13
charles-cohn <cohncl@gmatc.matc.edu>
2020-02-23 12:47:49 -0600

    Finished Assignment-3

---
+++ Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SingleLinkedListProject
+{
+    class Node
+    {
+        public int info;
+        public Node link;
+
+        public Node(int i)
+        {
+            info = i;
+            link = null;
+        }
+        
+    }
+}
